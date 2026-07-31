12e28663717c0f204e52d3e9fb6706ce9f3c7d5a
vamshi-987 <vamshivardhan987@gmail.com>
2026-07-31 18:08:09 +0530

    Add Expense entity

---
+++ src/main/java/com/vamshi/expense_tracker/entity/Expense.java
@@ -0,0 +1,34 @@
+package com.vamshi.expense_tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="expenses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String date;
+
+
+}
